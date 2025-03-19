39c048e88872668c18899ae5e53058f3eacc055f
RCafur <nandez.21155dlc@gmail.com>
2025-03-19 13:03:01 -0500

    Descripcion de los cambios

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 let contGrande = document.getElementById("grande-container");
 let puntos = document.getElementsByClassName("punto");
-let totalImagenes = contGrande.children.length; // Contar cuántas imágenes hay
-
-console.log(puntos);
+let totalImagenes = contGrande.children.length; 
+alert("Mueve el mouse lentamente por toda la pagina");
 
 for (let i = 0; i < puntos.length; i++) {
     puntos[i].addEventListener("click", () => {
         let posicionPunto = i;
-        let posicionContenedor = -(100 / totalImagenes) * posicionPunto; // Mueve de acuerdo con la cantidad de imágenes
+        let posicionContenedor = -(100 / totalImagenes) * posicionPunto; 
 
         contGrande.style.transform = `translateX(${posicionContenedor}%)`;
 
-        // Remover la clase "punto-activo" de todos
         for (let j = 0; j < puntos.length; j++) {
             puntos[j].classList.remove("punto-activo");
         }
 
-        // Agregar "punto-activo" al clickeado
         puntos[i].classList.add("punto-activo");
     });
 }
